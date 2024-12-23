fe082d1566b309507002b9f37542a4a9c8a3aad0
MorganK <morgank777777@gmail.com>
2024-12-23 16:36:06 +0800

    draw a graph

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ def solution(A):
 
 A = [2, 1, 1, 2, 3, 1]
 print(solution(A)) # 3
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+# 生成数据
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+# 绘制图形
+plt.plot(x, y)
+# 显示图形
+plt.show()
